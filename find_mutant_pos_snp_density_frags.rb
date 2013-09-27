4020272022e6932aced601e7c79adfeb0b6a0303
Edward Chalstrey <edward.chalstrey@tsl.ac.uk>
2013-09-27 12:01:18 +0100

    comparing densities3

--- find_mutant_pos_snp_density_frags.rb
+++ find_mutant_pos_snp_density_frags.rb
@@ -42,23 +42,25 @@ fasta.each do |frag|
 		x += 1
 	end
 end
-a = densities.zip(ids_w_snps)
-density_order = a.sort
+a = densities.zip(ids_w_snps) # array of density and id values, for the frags w snps
+density_order = a.sort 
 snp_ids_density_order = density_order.flatten.values_at(* density_order.flatten.each_index.select {|i| i.odd?})
-frags_by_density = []
+# selecting the odd values from the flattened density order array produces a list of frag ids ranked by density
+frags_by_density = [] 
 frags_by_density << ids_zero_snps #all of these frags have 0 snp density
 frags_by_density << snp_ids_density_order
-frags_by_density = frags_by_density.flatten
+frags_by_density = frags_by_density.flatten #all the frag ids now ranked by density with all the zero's at the start
 
-position_each_frag_id_in_d = fasta_ids.map{|x| frags_by_density.index(x)}
-index_values = Array(0..(position_each_frag_id_in_d.length - 1))
+position_each_frag_id_in_d = fasta_ids.map{|x| frags_by_density.index(x)} #works out the index of fasta_id values in frags_by_density
+index_values = Array(0..(fasta_ids.length - 1)) # index values that fasta_ids originally at
 both = []
 both << position_each_frag_id_in_d
 both << index_values
+difference = both.transpose.map {|x| x.reduce(:-)} #these differences will obviously be a lot, a first experiment
+# taking away old position from new position, to find the distance that the frag has moved when re-ordered by density
+# when doing the method above again for the reordered frags_by_density, all values will be 0, if order the same as frag_ids
+puts difference #DISTANCE FROM ORIGINAL POSITION - positive no. means moved forward in array, negative back
 
-difference = both.transpose.map {|x| x.reduce(:-)}
-puts difference #OK SO NEED TO CHECK THIS IS DOING WHAT I WANT IT TO!!! SHOULD BE DISTANCE FROM ORIGINAL POSITION
 
-#puts index_values
-#puts position_each_frag_id_in_d.length
+
 
