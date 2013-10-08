0a9637d8bad61e8be4f33d8998e53ee6fe243f7d
Edward Chalstrey <edward.chalstrey@tsl.ac.uk>
2013-10-08 16:10:50 +0100

    tidy methods and one json

--- fragments_w_snps.rb
+++ fragments_w_snps.rb
@@ -2,104 +2,151 @@ require "rubygems"
 require "rinruby"
 require "json"
 
-myr = RinRuby.new(echo = false)
-myr.eval "x <- rnorm(200000, 100000, 19000)"
-myr.eval "h <- hist(x, plot = FALSE)"
-myr.eval "b <- h$breaks"
-myr.eval "c <- h$counts"
-snp_counts = myr.pull "c"
-snp_breaks = myr.pull "b"
-#puts snp_counts.length # 18 counts because split into 20 lots of 10kb for 200kb sequence, 2 counts have 0 snps (one at either end of dist.)
-#puts snp_breaks.length # 19 breaks because these are the breaks between the 20 'bars' of the histogram
-snp_pos = [] #each of the ranges between each of our 'breaks' (between bars of the histogram) needs a proportion of snps
-x = Random.new
-i = 0
-snp_counts.each do |l| #for each count do
-	(snp_breaks[i].to_i..snp_breaks[i+1].to_i).each do |j| #for each range we need a random no. for each position (ranges in this case are 10k)
-		newr = x.rand(1..200000) #make a random no. within range 
-		if newr <= l
-			snp_pos << j #add the position (j) to array if its random no. (newr) is <= count. 
-			#count is frequency, so there should be count/200k snp positions in each range
+def generate_positions (breaks, counts)
+	snp_pos = [] #each of the ranges between each of our 'breaks' (between bars of the histogram) needs a proportion of snps
+	x = Random.new
+	i = 0
+	counts.each do |l| #for each count do
+		(breaks[i].to_i..breaks[i+1].to_i).each do |j| #for each range we need a random no. for each position (ranges in this case are 10k)
+			newr = x.rand(1..200000) #make a random no. within range 
+			if newr <= l
+				snp_pos << j #add the position (j) to array if its random no. (newr) is <= count. 
+				#count is frequency, so there should be count/200k snp positions in each range
+			end
+		end
+		i+=1
+	end
+	return snp_pos
+end
+def sample (snp_pos, rinruby)
+	rinruby.assign "pos_l", snp_pos #take 1000 snps (still normally distributed) to use
+	rinruby.eval "pos_s <- sample(pos_l, 1000)"
+	final_positions = rinruby.pull "pos_s"
+	return final_positions
+end
+def normal_dist
+	myr = RinRuby.new(echo = false)
+	myr.eval "x <- rnorm(200000, 100000, 19000)"
+	myr.eval "h <- hist(x, plot = FALSE)"
+	myr.eval "b <- h$breaks"
+	myr.eval "c <- h$counts"
+	c = myr.pull "c"
+	b = myr.pull "b"
+	#puts snp_counts.length # 18 counts because split into 20 lots of 10kb for 200kb sequence, 2 counts have 0 snps (one at either end of dist.)
+	#puts snp_breaks.length # 19 breaks because these are the breaks between the 20 'bars' of the histogram
+	snp_pos = generate_positions(b, c)
+	s = sample(snp_pos, myr)
+	return s
+end
+
+def unique? (snp_pos) #This method is an experiment/test to show that the previous methods have produced unique snp positions
+	unique_pos = []
+	snp_pos.each do |q|
+		if unique_pos.include? q
+			next
+		else
+			unique_pos << q
 		end
 	end
-	i+=1
+	puts "All snp positions generated are unique: " + (snp_pos.length == unique_pos.length).to_s
 end
-myr.assign "pos_l", snp_pos #take 1000 snps (still normally distributed) to use
-myr.eval "pos_s <- sample(pos_l, 1000)"
-final_positions = myr.pull "pos_s"
-unique_pos = []
-final_positions.each do |q|
-	if unique_pos.include? q
-		next
-	else
-		unique_pos << q
+
+def make_snp_seq (snp_pos)
+	snp_seq = ['a', 'g', 't', 'g', 'c']*40000 #This step is actually unecessary, though it shows later on when creating a vcf file that all works.
+	return snp_seq
+end
+def get_frags (seq, snp_pos)
+	frags = []
+	all_frags_pos = []
+	rt = 0
+	while rt < seq.length
+		frag_length = rand(200) + 50
+		frag = seq[rt..(rt+frag_length)]
+		#pos = snp_pos.select{|x| x>=rt and x<(rt+frag_length)}
+		rt = rt+frag_length
+		frags << frag
+		#all_frags_pos << pos
+	end
+	return frags#, all_frags_pos
+end
+def remove_extra_frags (frags)
+	x = frags.flatten.length - 200000
+	y = frags[-1].length
+	c = 2
+	while y < x
+		y = y + frags[-c].length
+		c+=1
+	end
+	frags2 = frags - frags[-c..-1]
+	leftover = frags.flatten.length - frags2.flatten.length
+	leftover2 = frags.flatten.length - 200000
+	final_frag = ['a']*(leftover - leftover2)
+	frags2 << final_frag
+	return frags2
+end
+def pos_each_frag (snp_pos, frags) #get the positions for snps on individual frags
+	sorted_pos = snp_pos.sort
+	p_ranges = []
+	frags.each do |i|
+		p_ranges << i.length + p_ranges[-1].to_i #adding the fragment lengths to get the upper bounds of ranges of positions on the original seq.
+	end
+	first_pos = [] #then, to work out the first position of each fragment
+	first_pos << 0
+	first_pos << p_ranges[0..-2]
+	first_pos = first_pos.flatten
+	p = 0
+	t = 0
+	all_frags_pos = [] # the positions of snps on each fragment, array of arrays
+	p_ranges.each do |jj| #for each of the upper ranges (j) that the positions could be in
+		each_frag_pos = []
+		while sorted_pos[t].to_i < jj && !sorted_pos[t].nil? do #make the loop quit before trying to access index of snp_pos that doesn't exist
+			each_frag_pos << sorted_pos[t].to_i 	# add all of the positions < jj to a new array for each frag
+			t += 1
+		end
+		z = 0
+		y = first_pos[p].to_i
+		each_frag_pos.each do |e|   
+			each_frag_pos[z] = e - y #taking away the value at the first position of each fragment to get the true positions
+			z += 1
+		end
+		p += 1
+		all_frags_pos << each_frag_pos # add the positions for the frag to an array of the each_frag arrays
+	end
+	return all_frags_pos
+end
+def storage (frags, pos)
+	frags_with_positions = []
+	x = 0
+	frags.each do |i|
+		frag_pos_hash = {}
+		frag_pos_hash[:frag] = i.join.to_s
+		frag_pos_hash[:pos] = pos[x]
+		frags_with_positions << frag_pos_hash  #so ideally i would use the hashed pos_each_frag method above, but for some reason dnt work
+		x+=1
+	end
+	return frags_with_positions
+end
+
+def write_json (array)
+	File.open("frags_with_positions.json", "w") do |f|
+		f.write(array.to_json)
 	end
 end
-puts "There are " + final_positions.length.to_s + " SNP\'s"
-puts unique_pos.length.to_s + " of the SNP positions are unique" #this proves each snp position is unique
-puts
-#Create sequence with SNPs at designated positions
-a = ['a', 'g', 't', 'g', 'a'] #This step is actually unecessary, though it shows later on when creating a vcf file that all works.
-snp_seq = a*200000
-unique_pos.each{
-	|i|
-	snp_seq[i] = 'c' #replacing the slots at the snp positions with snp's (always c for simplicity)
-}
 
-#THE LOOP BELOW WORKS, BUT IS VERY VERY SLOW
-#Split sequences into random sized fragments
-frags = []
-until frags.flatten.join.to_s.split(//).length >= 200000 do #once the frags array has all 200Kb from snp_seq
-	frags << (snp_seq.each_slice(rand(200) + 50).to_a)[0] #give one frag of 50-200b to the frags array
-	seq_string = snp_seq.join.to_s  #create a string from the sequence for slicing
-	frags_string = frags[-1].join.to_s #create a string of the most recently added fragment to slice off
-	seq_string.slice! frags_string #slice the fragment from the start of the string and... 
-	snp_seq = seq_string.split(//) #modify the sequence so it no longer contains the removed frags, then the loop can repeat
-	puts "Progress: " + ((frags.flatten.join.to_s.split(//).length)/2000).to_s + "%"
-end
-all = frags.flatten
-x = all.length - 200000
-(frags[-1])[-(x)..-1] = 'z'
-frags[-1].delete_if {|i| i > 'y'} #shorten the final fragment that has extra nucleotides due to the until >= 200Kb loop
-puts "Total nucleotides: " + (frags.flatten.join.to_s.split(//).length).to_s
-puts "There are a total of " + frags.length.to_s + " fragments"
+snp_pos = normal_dist #make a normal distribution of snp positions
+unique?(snp_pos) #are the positions generated unique?
 
-#Find the positions of snps on the fragments, based on the positions in the un-fragmented sequence
-sorted_pos = unique_pos.sort
-p_ranges = []
-frags.each do |i|
-	p_ranges << i.length + p_ranges[-1].to_i #adding the fragment lengths to get the upper bounds of ranges of positions on the original seq.
-end
-first_pos = [] #then, to work out the first position of each fragment
-first_pos << 0
-first_pos << p_ranges[0..-2]
-first_pos = first_pos.flatten
-p = 0
-t = 0
-all_frags_pos = [] # the positions of snps on each fragment, array of arrays
-p_ranges.each do |jj| #for each of the upper ranges (j) that the positions could be in
-	each_frag_pos = []
-	while sorted_pos[t].to_i < jj && !sorted_pos[t].nil? do #make the loop quit before trying to access index of snp_pos that doesn't exist
-		each_frag_pos << sorted_pos[t].to_i 	# add all of the positions < jj to a new array for each frag
-		t += 1
-	end
-	z = 0
-	y = first_pos[p].to_i
-	each_frag_pos.each do |e|   
-		each_frag_pos[z] = e - y #taking away the value at the first position of each fragment to get the true positions
-		z += 1
-	end
-	p += 1
-	all_frags_pos << each_frag_pos # add the positions for the frag to an array of the each_frag arrays
-end
-puts "The positions are separated into " + (all_frags_pos.length).to_s + " fragments"
-puts "There are " + (all_frags_pos.flatten.length).to_s + " positions containing SNPs"
+snp_seq = make_snp_seq(snp_pos) #make a sequence with snps at the positions^
+frags_n_pos = get_frags(snp_seq, snp_pos) #get an array of fragments, and an array of each of the positions associated with each fragment
 
-File.open("snp_pos.json", "w") do |f|
-	f.write(all_frags_pos.to_json)
-end
+frags = remove_extra_frags(frags_n_pos)
+#positions = frags_n_pos[1]
+positions_each = pos_each_frag(snp_pos, frags)
 
-File.open("frags.json", "w") do |f|
-	f.write(frags.to_json)
-end
+frags_with_positions = storage(frags, positions_each)
+write_json(frags_with_positions)
+
+
+
+
 
--- json->fasta.rb
+++ json->fasta.rb
@@ -1,27 +1,29 @@
 require "rubygems"
 require "json"
 
-def fasta_array (json)
-	frags = JSON.parse(File.open(json).read) #open the json containing an array of frags, each frag is an array of nucleotides
-	frag_strings = [] #sequences
+def extract_json (json)
+	snp_pos = []
+	frags = []
+	JSON.parse(File.open(json).read).each do |hash|
+		frags << hash["frag"]
+		snp_pos << hash["pos"]
+	end
+	return frags, snp_pos
+end
+def fasta_array (frags)
 	id_and_length = [] 
-	frag_ids = [] #create an id for each of the frags
 	x = 0
 	frags.each do |i|
-		frag_strings << i.join.to_s #add these to a new array of the sequences in string format
-		frag_ids << ('>frag' + (x+1).to_s)
 		id_and_length << (('>frag' + (x+1).to_s) + "  Length = " + i.length.to_s)
 		x+=1
 	end
-	fastaformat_array = id_and_length.zip(frag_strings) #create the array, each element of which goes onto a new line in fasta
-	return fastaformat_array, frags
+	fastaformat_array = id_and_length.zip(frags) #create the array, each element of which goes onto a new line in fasta
+	return fastaformat_array
 end
-
-def vcf_array (frags)
-	snp_pos = JSON.parse(File.open('snp_pos.json').read)#open the json containing the position data for the snps on each fragment - array of arrays
-	ids = []
+def vcf_array (frags, snp_pos)
 	x = 0
-	frags.each do |i| #getting the ids - removing the '>'
+	ids = []
+	frags.each do |i| #getting the ids 
 		ids << ('frag' + (x+1).to_s)
 		x+=1
 	end
@@ -29,7 +31,7 @@ def vcf_array (frags)
 	alt = []
 	q = 0
 	snp_pos.each do |h|
-		if h.to_s != '[]' #all of the fragments that contain at least one snp, TESTED = 100, so correct
+		if h.to_s != '[]' #all of the fragments that contain at least one snp
 			h.length.times do #*no. of snps
 				chrom << ids[q]
 			end
@@ -39,33 +41,43 @@ def vcf_array (frags)
 		end
 		q += 1
 	end
+	ref = []
+	alt.each do |base|
+		if base == 'A'
+			ref << 'T'
+		else
+			ref << 'A'
+		end
+	end
 	vcf_format = ['##fileformat=VCFv4.1', '##source=Fake', '#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO'] 
 	u = 0
 	snp_pos.flatten.each do |i|
-		line = chrom[u] + '	' + i.to_s + '	' + '.' + '	' + '.' + '	' + alt[u] + '	' + '100' + '	' + 'PASS' + '	' + '.'
+		line = chrom[u] + '	' + i.to_s + '	.	' +	ref[u] + '	' + alt[u] + '	100	PASS	.'
 		vcf_format << line
 		u += 1
 	end
 	return vcf_format
 end
-
 def write_fasta (array, file)
 	File.open(file, "w+") do |f|
 		array.each { |i| f.puts(i) } #write the fasta
 	end
 end
-
 def write_vcf (array, file)
 	File.open(file, "w+") do |f|
 		array.each { |i| f.puts(i) }
 	end
 end
 
-fasta_and_frags = fasta_array('frags.json')
-fastaformat_array = fasta_and_frags[0]
+data = extract_json('frags_with_positions.json')
+frags = data[0]
+snp_pos = data[1]
+
+fastaformat_array = fasta_array(frags)
 #fastaformat_array_shuf = fastaformat_array.shuffle #shuffle it for shits and giggles #YOU CAN USE THE SHUFFLED VERSION LATER ON
-frags = fasta_and_frags[1]
-vcf = vcf_array(frags)
+
+vcf = vcf_array(frags, snp_pos)
+
 write_fasta(fastaformat_array, 'frags.fasta')
 write_vcf(vcf, 'snps.vcf')
 
