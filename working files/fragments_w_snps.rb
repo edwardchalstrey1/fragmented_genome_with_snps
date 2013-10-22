2314a793e9661b1e4b43f5b2f9dce368c8f5a5f8
Edward Chalstrey <edwardchalstrey@gmail.com>
2013-10-22 17:09:49 +0100

    folders

---
+++ working files/fragments_w_snps.rb
@@ -0,0 +1,150 @@
+require "rubygems"
+require "rinruby"
+require "json"
+
+def generate_positions (breaks, counts)
+	snp_pos = [] #each of the ranges between each of our 'breaks' (between bars of the histogram) needs a proportion of snps
+	x = Random.new
+	i = 0
+	counts.each do |l| #for each count do
+		(breaks[i].to_i..breaks[i+1].to_i).each do |j| #for each range we need a random no. for each position (ranges in this case are 10k)
+			newr = x.rand(1..200000) #make a random no. within range 
+			if newr <= l
+				snp_pos << j #add the position (j) to array if its random no. (newr) is <= count. 
+				#count is frequency, so there should be count/200k snp positions in each range
+			end
+		end
+		i+=1
+	end
+	return snp_pos
+end
+def sample (snp_pos, rinruby)
+	rinruby.assign "pos_l", snp_pos #take 1000 snps (still normally distributed) to use
+	rinruby.eval "pos_s <- sample(pos_l, 1000)"
+	final_positions = rinruby.pull "pos_s"
+	return final_positions
+end
+def normal_dist
+	myr = RinRuby.new(echo = false)
+	myr.eval "x <- rnorm(200000, 100000, 19000)"
+	myr.eval "h <- hist(x, plot = FALSE)"
+	myr.eval "b <- h$breaks"
+	myr.eval "c <- h$counts"
+	c = myr.pull "c"
+	b = myr.pull "b"
+	#puts snp_counts.length # 18 counts because split into 20 lots of 10kb for 200kb sequence, 2 counts have 0 snps (one at either end of dist.)
+	#puts snp_breaks.length # 19 breaks because these are the breaks between the 20 'bars' of the histogram
+	snp_pos = generate_positions(b, c)
+	s = sample(snp_pos, myr)
+	return s
+end
+
+def unique? (snp_pos) #This method is an experiment/test to show that the previous methods have produced unique snp positions
+	unique_pos = []
+	snp_pos.each do |q|
+		if unique_pos.include? q
+			next
+		else
+			unique_pos << q
+		end
+	end
+	puts "All snp positions generated are unique: " + (snp_pos.length == unique_pos.length).to_s
+end
+
+def make_snp_seq (snp_pos)
+	snp_seq = ['a', 'g', 't', 'g', 'c']*40000 #This step is actually unecessary, though it shows later on when creating a vcf file that all works.
+	return snp_seq
+end
+def get_frags (seq, snp_pos)
+	frags = []
+	all_frags_pos = []
+	rt = 0
+	while rt < seq.length
+		frag_length = rand(200) + 50
+		frag = seq[rt..(rt+frag_length)]
+		#pos = snp_pos.select{|x| x>=rt and x<(rt+frag_length)}
+		rt = rt+frag_length
+		frags << frag
+		#all_frags_pos << pos
+	end
+	return frags#, all_frags_pos
+end
+def remove_extra_frags (frags)
+	x = frags.flatten.length - 200000
+	y = frags[-1].length
+	c = 2
+	while y < x
+		y = y + frags[-c].length
+		c+=1
+	end
+	frags2 = frags - frags[-c..-1]
+	leftover = frags.flatten.length - frags2.flatten.length
+	leftover2 = frags.flatten.length - 200000
+	final_frag = ['a']*(leftover - leftover2)
+	frags2 << final_frag
+	return frags2
+end
+def pos_each_frag (snp_pos, frags) #get the positions for snps on individual frags
+	sorted_pos = snp_pos.sort
+	p_ranges = []
+	frags.each do |i|
+		p_ranges << i.length + p_ranges[-1].to_i #adding the fragment lengths to get the upper bounds of ranges of positions on the original seq.
+	end
+	first_pos = [] #then, to work out the first position of each fragment
+	first_pos << 0
+	first_pos << p_ranges[0..-2]
+	first_pos = first_pos.flatten
+	p = 0
+	t = 0
+	all_frags_pos = [] # the positions of snps on each fragment, array of arrays
+	p_ranges.each do |jj| #for each of the upper ranges (j) that the positions could be in
+		each_frag_pos = []
+		while sorted_pos[t].to_i < jj && !sorted_pos[t].nil? do #make the loop quit before trying to access index of snp_pos that doesn't exist
+			each_frag_pos << sorted_pos[t].to_i 	# add all of the positions < jj to a new array for each frag
+			t += 1
+		end
+		z = 0
+		y = first_pos[p].to_i
+		each_frag_pos.each do |e|   
+			each_frag_pos[z] = e - y #taking away the value at the first position of each fragment to get the true positions
+			z += 1
+		end
+		p += 1
+		all_frags_pos << each_frag_pos # add the positions for the frag to an array of the each_frag arrays
+	end
+	return all_frags_pos
+end
+def storage (frags, pos)
+	frags_with_positions = []
+	x = 0
+	frags.each do |i|
+		frag_pos_hash = {}
+		frag_pos_hash[:frag] = i.join.to_s
+		frag_pos_hash[:pos] = pos[x]
+		frags_with_positions << frag_pos_hash  #so ideally i would use the hashed pos_each_frag method above, but for some reason dnt work
+		x+=1
+	end
+	return frags_with_positions
+end
+def write_json (array)
+	File.open("frags_with_positions.json", "w") do |f|
+		f.write(array.to_json)
+	end
+end
+
+snp_pos = normal_dist #make a normal distribution of snp positions
+unique?(snp_pos) #are the positions generated unique?
+
+snp_seq = make_snp_seq(snp_pos) #make a sequence with snps at the positions^
+frags_n_pos = get_frags(snp_seq, snp_pos) #get an array of fragments, and an array of each of the positions associated with each fragment
+
+frags = remove_extra_frags(frags_n_pos)
+#positions = frags_n_pos[1]
+positions_each = pos_each_frag(snp_pos, frags)
+
+frags_with_positions = storage(frags, positions_each)
+write_json(frags_with_positions)
+File.open("snp_pos.txt", "w+") do |f|
+	snp_pos.each { |i| f.puts(i) }
+end
+
---
+++ working files/rearrangement_methods.rb
@@ -0,0 +1,186 @@
+require 'rubygems'
+require 'bio-samtools'
+require 'bio'
+require "json"
+
+def extract_json (json)
+	output_array = JSON.parse(File.open(json).read)
+	return output_array
+end
+def score (frags_original_order, rearranged)
+	position_each_frag_id_in_d = frags_original_order.map{|x| rearranged.index(x)} #works out the index of fasta_id values in frags_by_density
+	index_values = Array(0..(frags_original_order.length - 1)) # index values that fasta_ids originally at
+	both = []
+	both << position_each_frag_id_in_d
+	both << index_values
+	difference = both.transpose.map {|x| x.reduce(:-)} # taking away old position from new position, to find the distance that the frag has moved when re-ordered
+	difference_abs = []
+	difference.each do |i|
+		difference_abs << i.abs
+	end
+	score = difference_abs.inject(:+) #high score = bad, score of 0 means the fragments in the right order
+	return score
+end
+def odds (array) #create array from odd indexes of input array
+	return array.values_at(*array.each_index.select {|i| i.odd?})
+end
+def evens (array)
+	return array.values_at(*array.each_index.select {|i| i.even?})
+end
+def two_a_to_h (keys_array, values_array)
+	return Hash[*keys_array.zip(values_array).flatten]
+end
+def density_order (frags_by_density, fasta_lengths, id_pos_hash) #get fasta lengths array and id/positions hash in SNP density order
+	d_lengths = []
+	d_pos = []
+	length_hash = two_a_to_h(id_pos_hash.keys, fasta_lengths) #make hash of ids in fasta file order with lengths
+	frags_by_density.each do |id|
+		d_pos << id_pos_hash[id]
+		d_lengths << length_hash[id] #the lengths of each fragment in the same order as frags_by_density (SNP density order)
+	end
+	d_id_pos_hash = two_a_to_h(frags_by_density, d_pos) #an id/positions hash where the ids are in SNP density order
+	return d_id_pos_hash, d_lengths
+end
+def re_order_densities (id_density_hash, rearranged_ids) # get list of the densities for a rearrangement of ids (frags)
+	densities = []
+	rearranged_ids.each do |id|
+		densities << id_density_hash[id]
+	end
+	return densities
+end
+def scatta_txts (array, name_string) # write each list of ids and densities into id_n_density_txts directory, for use in scatter plots in R
+	File.open(('id_n_density_txts/'+name_string+'.txt'), "w+") do |f|
+		array.each { |i| f.puts(i) }
+	end
+end
+# Rearrangement Methods
+# ---------------------
+
+# Control
+def random_score (frags_original_order, frags_by_density)
+	scores = []
+	100.times do |i|
+		scores << score(frags_original_order, frags_by_density.shuffle)
+	end
+	return scores
+end
+
+# Even/odd and odd/even
+def even_odd (frags_by_density, odd_even_string)
+	if odd_even_string == 'even' #even values then odd values reversed or opposite
+		rearranged = evens(frags_by_density) #the even numbers first half
+		rearranged << (odds(frags_by_density)).reverse #the odd numbers reversed second half 
+	else
+		rearranged = odds(frags_by_density) #the odd numbers first half
+		rearranged << (evens(frags_by_density)).reverse #the even numbers reversed second half 
+	end
+	return rearranged.flatten   
+end
+
+# Left/right method
+def left_right (id_pos_hash, fasta_lengths)
+	left = [] # frags with snps's mostly to the right, so should be on left of distribution
+	right = [] # frags with snps's mostly to the left, so should be on right of distribution
+	d = 0
+	id_pos_hash.keys.each do |x| # for each of the frag ids
+		temp = []
+		(id_pos_hash[x].split(",").map { |s| s.to_i }).each do |i| # adding each of the positions associated with that frag/key to temporary array
+			temp << i
+		end
+		sum = temp.inject(:+) # summing the positions
+		if sum != nil # ignoring the fragments with 0 snps, which have no postions, so sum = nil
+			lxnp = (fasta_lengths[d]*temp.length)/2 # then working out half the length of the fragment, multiplied by the number of positions
+			if sum < lxnp # if the sum of the positions is < half the length of the fragment, multiplied by the number of positions
+				# this is equivalent to working out the average position and checking it against half the length
+				right << x
+			else
+				left << x
+			end
+		else #frags with 0 snps
+			if evens(id_pos_hash.keys).include?(x) 
+				left << x
+			else
+				right << x #add odd indexed frags with 0 snps to right, and evens to left
+			end
+		end 
+		d+=1 
+	end
+	return left, right
+end
+def lr_d (frags_by_density, fasta_lengths, id_pos_hash)
+	density_order_data = density_order(frags_by_density, fasta_lengths, id_pos_hash)
+	d_id_pos_hash = density_order_data[0]
+	d_lengths = density_order_data[1]
+	lnr = left_right(d_id_pos_hash, d_lengths) #get the left/right arrays for density ordered frags so both arrays are still ordered by density
+	return lnr[0], lnr[1].reverse # reverse the right array into descending density order
+end
+
+def max_score (original_order)
+end
+
+frags_original_order = extract_json('frag_ids_original_order.json')
+frags_reverse_order = frags_original_order.reverse
+frags_by_density = extract_json('frags_by_density.json')
+
+id_pos_hash = extract_json('pos_hash.json') #remember the fragments are in a random order here - the order they were in in the fasta
+fasta_lengths = extract_json('fasta_lengths.json') #also in the order from the fasta file
+
+id_density_hash = extract_json('id_density_hash.json')
+
+# Rearranged orders of densities: for scatter plots
+#-----------------------------------------------------------
+
+#Original order
+scatta_txts(re_order_densities(id_density_hash, frags_original_order), 'd_o')
+
+#Density C1
+scatta_txts(re_order_densities(id_density_hash, frags_by_density), 'd_c1')
+
+#Random C2 (example)
+id_c2 = frags_by_density.shuffle
+scatta_txts(re_order_densities(id_density_hash, id_c2), 'd_c2')
+
+#Even Odd M1 a/b
+id_m1a = even_odd(frags_by_density, 'even')
+scatta_txts(re_order_densities(id_density_hash, id_m1a), 'd_m1a')
+
+id_m1b = even_odd(frags_by_density, 'odd')
+scatta_txts(re_order_densities(id_density_hash, id_m1b), 'd_m1b')
+
+#Left Right M2 a: l, r, both
+lnr = left_right(id_pos_hash, fasta_lengths)
+
+id_m2al = lnr[0]
+scatta_txts(re_order_densities(id_density_hash, id_m2al), 'd_m2al')
+
+id_m2ar = lnr[1]
+scatta_txts(re_order_densities(id_density_hash, id_m2ar), 'd_m2ar')
+
+id_m2a = lnr.flatten
+scatta_txts(re_order_densities(id_density_hash, id_m2a), 'd_m2a')
+
+# Left Right Density M2 b: l, r, both
+lnrd = lr_d(frags_by_density, fasta_lengths, id_pos_hash)
+
+id_m2bl = lnrd[0]
+scatta_txts(re_order_densities(id_density_hash, id_m2bl), 'd_m2bl')
+
+id_m2br = lnrd[1]
+scatta_txts(re_order_densities(id_density_hash, id_m2br), 'd_m2br')
+
+id_m2b = lnrd.flatten
+scatta_txts(re_order_densities(id_density_hash, id_m2b), 'd_m2b')
+
+puts 'Highest possible score: ' + (score(frags_reverse_order, frags_original_order)).to_s
+puts 'Density order Score: ' + (score(frags_original_order, frags_by_density)).to_s
+random_scores = random_score(frags_original_order, frags_by_density)
+puts 'Random Score: ' + (random_scores.inject(:+)/random_scores.length).to_s
+puts 'Even Odd Method Score: ' + (score(frags_original_order, (even_odd(frags_by_density, 'even')))).to_s
+puts 'Odd Even Method Score: ' + (score(frags_original_order, (even_odd(frags_by_density, 'odd')))).to_s
+puts 'Left Right Method Score: ' + (score(frags_original_order, (left_right(id_pos_hash, fasta_lengths).flatten))).to_s
+puts 'Left Right Density Method Score: ' + (score(frags_original_order, (lr_d(frags_by_density, fasta_lengths, id_pos_hash).flatten))).to_s
+
+
+File.open('random_scores.txt', "w+") do |f|
+	(random_score(frags_original_order, frags_by_density)).each { |i| f.puts(i) } # get random scores for error bar creation
+end
