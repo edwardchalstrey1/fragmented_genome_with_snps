3d24d690d553fbd804358a5d6d875609c3339223
Edward Chalstrey <edward.chalstrey@tsl.ac.uk>
2013-10-24 16:07:03 +0100

    yes

--- working files/munkres_test.rb
+++
@@ -1,5 +0,0 @@
-require 'munkres'
-cost_matrix = [[3,2,1,0],[2,3,2,1],[1,2,3,2],[0,1,2,3]]
-m = Munkres.new(cost_matrix)
-p = m.find_pairings
-puts p.sort.to_s # gives an array of arrays, the first value in each sub-array is the original position, the second is the new position
