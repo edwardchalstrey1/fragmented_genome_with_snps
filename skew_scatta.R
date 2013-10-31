23eaff7c788e69aa321f5a23410392be0a426026
Edward Chalstrey <edwardchalstrey@gmail.com>
2013-10-31 17:05:50 +0000

    example gradient

--- skew_scatta.R
+++ skew_scatta.R
@@ -7,12 +7,19 @@ for(i in lengths){
 	s <- as.character(z[n])
 	y <- as.vector(as.matrix(read.table(paste("~/fragmented_genome_with_snps/skew_scatter/snps", s, ".txt", sep=""), quote="\"")))
 	x <- 1:(length(y))
-	mod <- lm(y ~ x)
-	gradients[n] <- abs(coef(mod)[2]) #gradient
+	gradients[n] <- abs(coef(lm(y ~ x))[2]) #gradient
 	n <- n+1
 }
+
 png("~/fragmented_genome_with_snps/figures/skew_scatter.png")
-plot(z, gradients, main="Graph to show the absolute gradient of SNPs for each fragment", xlab="Fragments", ylab="Gradient (as an absolute)")
+plot(z, gradients, main="The absolute gradient of SNPs for each fragment in dataset 2", xlab="Fragments", ylab="Gradient (as an absolute)")
 lo <- loess(y~x)
 lines(predict(lo), col="red", lwd=4)
+dev.off()
+
+png("~/fragmented_genome_with_snps/figures/example_gradient_f629.png")
+y <- as.vector(as.matrix(read.table(paste("~/fragmented_genome_with_snps/skew_scatter/snps629.txt", sep=""), quote="\"")))
+x <- 1:(length(y))
+plot(x, y, main="Example of gradient determination for fragment 629 of dataset 2", xlab="Nucleotides", ylab="Number of SNPs")
+abline(lm(y ~ x), col="blue", lwd=4)
 dev.off()
