41d2067b85ec1c82e6a90df5f277ca5616f5579c
Edward Chalstrey <edwardchalstrey@gmail.com>
2013-11-12 17:14:46 +0000

    arab scatta

--- skew_scatta.R
+++ skew_scatta.R
@@ -1,40 +1,42 @@
-lengths <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/skew_scatter/ex_fasta_lengths.txt", quote="\"")))
-ids <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/skew_scatter/ex_ids_w_snps.txt", quote="\"")))
+lengths <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/ex_fasta_lengths.txt", quote="\""))) #frags_w_snps
+ids <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/ex_ids_w_snps.txt", quote="\""))) #frags_w_snps
 w <- length(lengths) #number of fragments with snps
 gradients <- rep(NA, w)
 abs_gradients <- rep(NA, w)
 xs <- rep(NA, w) #make vectors of each x and y value
 ys <- rep(NA, w)
 q <- 1
-for(i in lengths){
+nu_ids <- c()
+for(i in lengths){ #for each fragment
 	s <- as.character(ids[q])
-	p <- as.vector(as.matrix(read.table(paste("~/fragmented_genome_with_snps/skew_scatter/snps", s, ".txt", sep=""), quote="\"")))
-	if (length(p) != 1){ # eliminating frags with just 1 SNP
-		a <- density(p, n=(i/5)) #THE DENSITY ESTIMATE DOESNT WORK BECAUSE SOME FRAGS HAVE ONLY 1 SNP, if you only included frags with 2+ SNPs it could work
+	p <- as.vector(as.matrix(read.table(paste("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/snps", s, ".txt", sep=""), quote="\"")))
+	if (length(p) < 6){ # eliminating frags with less than 6 SNPs
+		nu_ids <- ids[q] # add the id to a new vector, if it has 6 or more SNPs
+		a <- density(p, n=i) #dividing the length of the fragment by a constant (5) to get the number of equally spaced points at which the density is to be estimated
 		#^ whilst n varies for each fragment, the size of each section is the same for each frag
 		e <- c(which(a$x > i), which(a$x <= 0)) #vector of indices which are greater than the length or <=0. need to loop and remove each of these indices from a$x and a$y
 		for (j in e){
-			a$x[j] <- NA
+			a$x[j] <- NA # making those redundant indices NA
 			a$y[j] <- NA
 		}
-		x <- a$x[!is.na(a$x)]
+		x <- a$x[!is.na(a$x)] #removing indices that are NA
 		y <- a$y[!is.na(a$y)]
 	}
 	#xs[q] <- x
 	#ys[q] <- y
-	abs_gradients[q] <- abs(coef(lm(y ~ x))[2]) #gradients - NO LONGER IN SAME ORDER AS ID, ONES FROM FRAGS WITH 1 SNP MISSING
+	abs_gradients[q] <- abs(coef(lm(y ~ x))[2]) #gradients - should now be in same order as nu_ids
 	gradients[q] <- coef(lm(y ~ x))[2]
 	q <- q+1
 }
 
-png("~/fragmented_genome_with_snps/figures/figures_d2a/skew_scatter.png")
+png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/skew_scatter.png")
 plot(ids, abs_gradients, main="The absolute gradient of SNPs for each fragment in dataset 2", xlab="Fragments", ylab="Gradient (as an absolute)") 
-lines(predict(loess(abs_gradients~ids)), col="red", lwd=4)
+lines(predict(loess(abs_gradients~nu_ids)), col="red", lwd=4)
 dev.off()
 
-png("~/fragmented_genome_with_snps/figures/figures_d2a/skew_scatter2.png")
+png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/skew_scatter2.png")
 plot(ids, gradients, main="The gradient of SNPs for each fragment in dataset 2", xlab="Fragments", ylab="Gradient")
-lines(predict(loess(gradients~ids)), col="red", lwd=4)
+lines(predict(loess(gradients~nu_ids)), col="red", lwd=4)
 dev.off()
 
 #png("~/fragmented_genome_with_snps/figures/figures_d2a/example_gradient_f629.png")
---
+++ writeup/p3_rearrangement_methods.md
@@ -0,0 +1,32 @@
+Rearrangement methods part 3
+========================================================
+
+Dataset3:
+
+Highest possible score: 766322
+Density order Score: 472556
+Random Score: 511235
+Even Odd Method Score: 415488
+Odd Even Method Score: 416536
+Left Right Method Score: 512072
+Left Right Density Method Score: 415942
+
+Dataset4:
+
+Highest possible score: 771282
+Density order Score: 471278
+Random Score: 513873
+Even Odd Method Score: 422216
+Odd Even Method Score: 417454
+Left Right Method Score: 509716
+Left Right Density Method Score: 416990
+
+Dataset5:
+
+Highest possible score: 761378
+Density order Score: 475508
+Random Score: 508277
+Even Odd Method Score: 413846
+Odd Even Method Score: 411760
+Left Right Method Score: 506632
+Left Right Density Method Score: 414378
