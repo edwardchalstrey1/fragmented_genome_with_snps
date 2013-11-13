08e19bc0ee3b80989a7bab3d5efa861256573649
Edward Chalstrey <edwardchalstrey@gmail.com>
2013-11-13 13:29:33 +0000

    skew scatter arab d3

--- skew_scatta.R
+++ skew_scatta.R
@@ -1,18 +1,19 @@
 lengths <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/ex_fasta_lengths.txt", quote="\""))) #frags_w_snps
 ids <- as.vector(as.matrix(read.table("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/ex_ids_w_snps.txt", quote="\""))) #frags_w_snps
 w <- length(lengths) #number of fragments with snps
-gradients <- rep(NA, w)
-abs_gradients <- rep(NA, w)
-xs <- rep(NA, w) #make vectors of each x and y value
-ys <- rep(NA, w)
+gradients <- c()
+abs_gradients <- c()
+xs <- c() #make vectors of each x and y value
+ys <- c()
 q <- 1
+n <- 1
 nu_ids <- c()
 for(i in lengths){ #for each fragment
 	s <- as.character(ids[q])
 	p <- as.vector(as.matrix(read.table(paste("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/skew_scatter/snps", s, ".txt", sep=""), quote="\"")))
-	if (length(p) < 6){ # eliminating frags with less than 6 SNPs
-		nu_ids <- ids[q] # add the id to a new vector, if it has 6 or more SNPs
-		a <- density(p, n=i) #dividing the length of the fragment by a constant (5) to get the number of equally spaced points at which the density is to be estimated
+	if (length(p) > 6){ # eliminating frags with less than 6 SNPs
+		nu_ids <- c(nu_ids, ids[q]) # add the id to a new vector, if it has 6 or more SNPs
+		a <- density(p, n=i/5) #dividing the length of the fragment by a constant (5) to get the number of equally spaced points at which the density is to be estimated
 		#^ whilst n varies for each fragment, the size of each section is the same for each frag
 		e <- c(which(a$x > i), which(a$x <= 0)) #vector of indices which are greater than the length or <=0. need to loop and remove each of these indices from a$x and a$y
 		for (j in e){
@@ -21,30 +22,39 @@ for(i in lengths){ #for each fragment
 		}
 		x <- a$x[!is.na(a$x)] #removing indices that are NA
 		y <- a$y[!is.na(a$y)]
+
+		xs[[n]] <- x
+		ys[[n]] <- y
+		n <- n+1
+
+		coeff <- coef(lm(y ~ x))
+		abs_gradients <- c(abs_gradients, abs(coeff[2])) #gradients - should now be in same order as nu_ids
+		gradients <- c(gradients, coeff[2])
 	}
-	#xs[q] <- x
-	#ys[q] <- y
-	abs_gradients[q] <- abs(coef(lm(y ~ x))[2]) #gradients - should now be in same order as nu_ids
-	gradients[q] <- coef(lm(y ~ x))[2]
 	q <- q+1
 }
 
 png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/skew_scatter.png")
-plot(ids, abs_gradients, main="The absolute gradient of SNPs for each fragment in dataset 2", xlab="Fragments", ylab="Gradient (as an absolute)") 
-lines(predict(loess(abs_gradients~nu_ids)), col="red", lwd=4)
+plot(nu_ids, abs_gradients, main="The absolute gradient of SNPs for each fragment in dataset 3", xlab="Fragments", ylab="Gradient (as an absolute)") 
+lines(lowess(nu_ids, abs_gradients), col="red", lwd=4)
 dev.off()
 
 png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/skew_scatter2.png")
-plot(ids, gradients, main="The gradient of SNPs for each fragment in dataset 2", xlab="Fragments", ylab="Gradient")
-lines(predict(loess(gradients~nu_ids)), col="red", lwd=4)
+plot(nu_ids, gradients, main="The gradient of SNPs for each fragment in dataset 3", xlab="Fragments", ylab="Gradient")
+lines(lowess(nu_ids, gradients), col="red", lwd=4)
 dev.off()
 
-#png("~/fragmented_genome_with_snps/figures/figures_d2a/example_gradient_f629.png")
-#plot(xs[629], ys[629], main="Example of gradient determination for fragment 629 of dataset 2", xlab="Nucleotides", ylab="Number of SNPs")
-#abline(lm(ys[629] ~ xs[629]), col="blue", lwd=4)
-#dev.off()
+png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/example_gradient_f687.png")
+z <- which(687 == nu_ids)
+plot(xs[[z]], ys[[z]], main="Example of gradient determination for fragment 687 of dataset 3", xlab="Fragment", ylab="SNP density") 
+dev.off()
 
-#png("~/fragmented_genome_with_snps/figures/figures_d2a/example_gradient_f321.png")
-#plot(xs[321], ys[321], main="Example of gradient determination for fragment 321 of dataset 2", xlab="Nucleotides", ylab="Number of SNPs")
-#abline(lm(ys[321] ~ xs[321]), col="blue", lwd=4)
-#dev.off()
+png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/example_gradient_f318.png")
+zz <- which(318 == nu_ids)
+plot(xs[[zz]], ys[[zz]], main="Example of gradient determination for fragment 318 of dataset 3", xlab="Fragment", ylab="SNP density")
+dev.off()
+
+png("~/fragmented_genome_with_snps/arabidopsis_datasets/dataset3/figures/example_gradient_f1043.png")
+zzz <- which(1043 == nu_ids)
+plot(xs[[zzz]], ys[[zzz]], main="Example of gradient determination for fragment 1043 of dataset 3", xlab="Fragment", ylab="SNP density")
+dev.off()
