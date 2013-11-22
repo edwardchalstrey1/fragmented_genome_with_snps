30503ffb8f49a8fcfac6cc14b025a06083adffa7
Edward Chalstrey <edward.chalstrey@tsl.ac.uk>
2013-11-22 16:40:31 +0000

    get density method to work properly

--- skew_scatta.rb
+++ skew_scatta.rb
@@ -1,6 +1,7 @@
 require "rubygems"
 require "rinruby"
 
+
 lengths = []
 File.open("arabidopsis_datasets/dataset5/skew_scatter/ex_fasta_lengths.txt").each {|line| lengths << line}
 ids = []
@@ -16,6 +17,7 @@ lengths.each do |length|
 	File.open("arabidopsis_datasets/dataset5/skew_scatter/snps"+ids[q].to_s+".txt").each {|line| snp_pos << line}
 	if snp_pos.length > 6
 		nu_ids << ids[q]
+		x = density(snp_pos, length, )
 		#make x and y
 		xs << x
 		ys << y
