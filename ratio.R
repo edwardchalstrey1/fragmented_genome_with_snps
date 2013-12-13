8ad7384876ea3faf47d3e4a14d461d7a9f5d709a
Edward Chalstrey <edwardchalstrey@gmail.com>
2013-12-13 16:13:55 +0000

    useful ratio plot

--- ratio.R
+++ ratio.R
@@ -1,5 +1,14 @@
-hm <- rnorm(70, 10000000, 3000000)
-ht1 <- rnorm(350, 5000000, 1000000)
-ht2 <- rnorm(350, 15000000, 1000000)
+hm <- rnorm(35, 10000000, 5000000)
+ht1a <- rnorm(1500, 5000000, 1000000)
+ht1 <- ht1a[which(ht1a < 7.5e+06)] #non-recombinant region = 7.5m-12.5m
+ht2a <- rnorm(1500, 15000000, 1000000)
+ht2 <- ht2a[which(ht2a > 1.25e+07)] #non-recombinant region = 7.5m-12.5m
 ht <- c(ht1, ht2)
-plot(1:18585056, density(hm, n=18585056)$y/density(ht, n=18585056)$y)
+
+hmd <- density(hm, from=0, to=18585056)
+htd <- density(ht, from=0, to=18585056)
+x <- (1:512)*36298
+y <- hmd$y/htd$y
+plot(x, y)
+
+# I think this will do
