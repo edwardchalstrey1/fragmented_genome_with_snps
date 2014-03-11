57f4817d912cfbd95559fdea675fb13db9dc9533
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-03-11 17:04:54 +0000

    comments

--- create_model_genome.rb
+++ create_model_genome.rb
@@ -2,9 +2,18 @@
 require_relative 'lib/arabidopsis_c4_w_snps'
 require_relative 'lib/write_it'
 
-Dir.mkdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}")) # make the directory to put data files into
+=begin
+	
+Use a uniform distribution for the heterozygous snps http://astrostatistics.psu.edu/su07/R/html/stats/html/Uniform.html
+	
+=end
 
-snpz = ModelGenome::normal_dist
+
+# make the directory to put data files into
+Dir.mkdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}"))
+
+# Create the lists of homozygous and heterozygous SNPs
+snpz = ModelGenome::snp_dist
 snp_pos = snpz[0]
 hm = snpz[1]
 ht = snpz[2]
@@ -20,6 +29,7 @@ puts "Arabidopsis chr4 length: #{arabidopsis_c4.length} bases"
 puts "Fragmented seq   length: #{frags.join.length} = close enough? You decide."
 puts "You have created #{frags.length} fragments of sizes #{contig_size}-#{contig_size*2}"
 
+# Get the positions of the SNPs on fragments
 pos_on_all = ModelGenome::pos_each_frag(snp_pos, frags)
 pos_on_frags = pos_on_all[0]
 snp_pos_all = pos_on_all[1]
