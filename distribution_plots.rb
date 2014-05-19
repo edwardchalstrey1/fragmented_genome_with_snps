5bac436fde6698989b5fa0609f282531c00c326b
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-05-19 16:35:04 +0100

    removed extra require statements

--- distribution_plots.rb
+++ distribution_plots.rb
@@ -1,6 +1,4 @@
 #encoding: utf-8
-require_relative 'lib/score_plots/example_perms'
-require_relative 'lib/snp_dist'
 require_relative 'lib/reform_ratio'
 require_relative 'lib/write_it'
 
@@ -13,7 +11,9 @@ ARGV's: 0 = dataset
 
 genome_length = ReformRatio::genome_length("arabidopsis_datasets/#{ARGV[0]}/frags.fasta")
 
-Dir.chdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}/#{ARGV[1]}/Gen#{ARGV[2]}_lists")) do
-	perm_ratio = WriteIt.file_to_floats_array("gen_#{ARGV[2]}_hyp.txt")
-	SNPdist::plot_hyp(perm_ratio, "fragmented_genome_with_snps/arabidopsis_datasets", "#{ARGV[0]}/#{ARGV[1]}", ARGV[2], genome_length)
+Array(1..ARGV[2].to_i).each do |i|
+	Dir.chdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}/#{ARGV[1]}/Gen#{i}_lists")) do
+		perm_ratio = WriteIt.file_to_floats_array("gen_#{i}_hyp.txt")
+		SNPdist::plot_hyp(perm_ratio, "fragmented_genome_with_snps/arabidopsis_datasets", "#{ARGV[0]}/#{ARGV[1]}", i, genome_length)
+	end
 end
