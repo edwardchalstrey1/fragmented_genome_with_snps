4a870d56f58a117155e3a62d49085689e148e416
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-05-21 10:41:22 +0100

    generated performance figures for unfinished cluster run p_run51 of 10K_dataset3

--- distribution_plots.rb
+++ distribution_plots.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 require_relative 'lib/reform_ratio'
 require_relative 'lib/write_it'
+require_relative 'lib/snp_dist'
 
 =begin
 	
@@ -11,7 +12,7 @@ ARGV's: 0 = dataset
 
 genome_length = ReformRatio::genome_length("arabidopsis_datasets/#{ARGV[0]}/frags.fasta")
 
-Array(1..ARGV[2].to_i).each do |i|
+Array(0..ARGV[2].to_i).each do |i|
 	Dir.chdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{ARGV[0]}/#{ARGV[1]}/Gen#{i}_lists")) do
 		perm_ratio = WriteIt.file_to_floats_array("gen_#{i}_hyp.txt")
 		SNPdist::plot_hyp(perm_ratio, "fragmented_genome_with_snps/arabidopsis_datasets", "#{ARGV[0]}/#{ARGV[1]}", i, genome_length)
---
+++ examples_close_to_correct_permutation.rb
@@ -0,0 +1,30 @@
+#encoding: utf-8
+require_relative 'lib/score_plots/example_perms.rb'
+require_relative 'lib/score_plots/score_plots.rb'
+
+### Example plots ###
+
+location = "fragmented_genome_with_snps/arabidopsis_datasets"
+dataset = ARGV[0]
+run = ARGV[1]
+
+div = ARGV[5].to_f # Number of divisions at which to calculate SNP density in permutation
+n = ARGV[6].to_i # Number of permutations in each population (mutants of the original order, and random shuffles)
+
+snp_data = ReformRatio::get_snp_data("arabidopsis_datasets/#{dataset}/snps.vcf")
+fasta = ReformRatio::fasta_array("arabidopsis_datasets/#{dataset}/frags.fasta")
+
+genome_length = ReformRatio::genome_length("arabidopsis_datasets/#{dataset}/frags.fasta")
+hm = WriteIt::file_to_ints_array("#{Dir.home}/#{location}/#{dataset}/hm_snps.txt") # we can use the SNPs from the model genome to make example ratio
+ht = WriteIt::file_to_ints_array("#{Dir.home}/#{location}/#{dataset}/ht_snps.txt")
+fratio_breaks = SNPdist::fratio(hm, ht, div, genome_length) # frequency ratio array # 10,000 for 10K dataset
+comparable_ratio = SNPdist::hyp_snps(fratio_breaks, div, genome_length) # hypothetical snp positions array
+
+example_perms = ExamplePerms::get_perms(fasta, n, snp_data, comparable_ratio, div, genome_length) 
+
+MetricPlot::metric_plot(0, 2, 'dev', '10_chunk_swap_shuf_dev', example_perms, dataset, run)
+MetricPlot::metric_plot(0, 2, 'sq', '10_chunk_swap_shuf_sq', example_perms, dataset, run)
+MetricPlot::metric_plot(0, 2, 'ham', '10_chunk_swap_shuf_ham', example_perms, dataset, run)
+MetricPlot::metric_plot(0, 2, 'r', '10_chunk_swap_shuf_r', example_perms, dataset, run)
+MetricPlot::metric_plot(0, 2, 'lcs', '10_chunk_swap_shuf_lcs', example_perms, dataset, run)
+MetricPlot::metric_plot(0, 2, 'kt', '10_chunk_swap_shuf_kt', example_perms, dataset, run)
