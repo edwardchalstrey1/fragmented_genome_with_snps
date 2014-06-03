da21c3c15adcf1ea98e5dc1da75bba2875294569
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-03 17:15:02 +0100

    spread across 4 suites

--- parallel_runs_10K4.sh
+++ parallel_runs_10K4.sh
@@ -11,9 +11,9 @@ source ruby-2.0.0
 
 for i in {1..10}
 do
-	bsub -q TSL-Test128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$i 10000000000 100 50 35 35 25 5 1000.0 restart"
-	bsub -q TSL-Test128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$(($i+10)) 10000000000 20 10 7 7 5 1 1000.0 restart"
-	bsub -q TSL-Test128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$(($i+20)) 10000000000 50 25 20 20 6 4 1000.0 restart"
+	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$i 10000000000 100 50 35 35 25 5 1000.0 restart"
+	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$(($i+10)) 10000000000 20 10 7 7 5 1 1000.0 restart"
+	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$(($i+20)) 10000000000 50 25 20 20 6 4 1000.0 restart"
 	bsub -q TSL-Test256 -We 10000 "ruby algorithm_test.rb 10K_dataset4 p_run$(($i+30)) 10000000000 20 4 9 9 1 1 1000.0 restart"
 done
 
