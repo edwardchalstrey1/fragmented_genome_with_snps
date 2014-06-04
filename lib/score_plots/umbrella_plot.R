e03ae31ef978d288711f0a5d3a5e905454e4868f
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-04 10:46:19 +0100

    started new ggplot

--- lib/score_plots/umbrella_plot.R
+++ lib/score_plots/umbrella_plot.R
@@ -1,11 +1,17 @@
-uplot <- function(generations, metric_scores, group){
+uplot <- function(generations, metric_scores, run){
   library(ggplot2)
   df <- data.frame(
     x = factor(generations),
     y = metric_scores,
-    group = factor(group)
+    group = factor(run)
     )
   ggplot(df, aes(colour = group, y = y, x = x)) +
     geom_point(aes(y = y)) +
-    geom_smooth(aes(y = y), method="loess") # choose most appropriate smoothing method 
+    geom_smooth(aes(y = y, group=group), method="loess") # choose most appropriate smoothing method 
 }
+
+mets <- c(0.55, 0.60, 0.70, 0.65, 0.70, 0.80, 0.30, 0.40, 0.50, 0.40, 0.50, 0.60)
+gens <- c(1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2)
+groups <- c("run1", "run1", "run1", "run1", "run1", "run1", "run2", "run2", "run2", "run2", "run2", "run2")
+
+uplot(gens,mets,groups)
