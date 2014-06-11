92c7f61936174e016830256c75ec29a7a163c31f
Dan MacLean <maclean.daniel@gmail.com>
2014-06-11 17:01:50 +0100

    Type checking and redundancy removed

--- lib/write_it.rb
+++ lib/write_it.rb
@@ -20,25 +20,28 @@ class WriteIt
 	# Input: file location
 	# Output: array with each line of the file as an entry
 	def self.file_to_array(file)
-		array = []
-		IO.foreach(file) { |line| array << line.gsub(/\n/,'') }
-		array
+		IO.foreach(file).collect {|l| l.chomp }
 	end
 
 	# Input: file location (file must have integers on each line)
 	# Output: array with each line of the file as an entry, converted to integer
 	def self.file_to_ints_array(file)
-		array = []
-		IO.foreach(file) { |line| array << line.gsub(/\n/,'').to_i }
-		array
+	  begin 
+      WriteIt.file_to_array(file).collect {|l| Integer(l) }
+    resuce ArgumentError
+      $stderr.puts "Not all lines in file can be converted to ints"
+      exit
+    end
 	end
 
 	# Input: file location (file must have floats on each line)
 	# Output: array with each line of the file as an entry, converted to float
 	def self.file_to_floats_array(file)
-		array = []
-		IO.foreach(file) { |line| array << line.gsub(/\n/,'').to_f }
-		array
+    begin 
+      WriteIt.file_to_array(file).collect {|l| Float(l)}
+    rescue ArgumentError
+      $stderr.puts "Not all lines in file can be converted to ints"
+    end
 	end
 end
 
