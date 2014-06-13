26e02ffa927f1fd357d66f2f08eda53c7930acba
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-13 14:52:03 +0100

    figures can be made slightly quicker a second time

--- multiple_runs_performance.rb
+++ multiple_runs_performance.rb
@@ -10,57 +10,57 @@ original_order = ReformRatio.fasta_id_n_lengths(fasta)[0]
 title = 'Many replicate runs of GATOC with varying parameter groupings'
 
 gens, fits, runs, perms, param_types = UPlot.plot_info(dataset)
-# UPlot.uplot(dataset, gens, fits, runs, param_types, 'big_plot_fit', 'Fitness (compliment)', title)
+UPlot.uplot(dataset, gens, fits, runs, param_types, 'big_plot_fit', 'Fitness (compliment)', title)
 
-# deviation_distances, x = [], 1
-# perms.each do |perm|
-# 	deviation_distances << PDist.deviation(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, deviation_distances, runs, param_types, 'big_plot_dev', 'Deviation distance (normalized)', title)
+deviation_distances, x = [], 1
+perms.each do |perm|
+	deviation_distances << PDist.deviation(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, deviation_distances, runs, param_types, 'big_plot_dev', 'Deviation distance (normalized)', title)
 
-# square, x = [], 1
-# perms.each do |perm|
-# 	square << PDist.square(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, square, runs, param_types, 'big_plot_square', 'Square deviation distance (normalized)', title)
+square, x = [], 1
+perms.each do |perm|
+	square << PDist.square(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, square, runs, param_types, 'big_plot_square', 'Square deviation distance (normalized)', title)
 
-# hamming, x = [], 1
-# perms.each do |perm|
-# 	hamming << PDist.hamming(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, hamming, runs, param_types, 'big_plot_hamming', 'Hamming distance (normalized)', title)
+hamming, x = [], 1
+perms.each do |perm|
+	hamming << PDist.hamming(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, hamming, runs, param_types, 'big_plot_hamming', 'Hamming distance (normalized)', title)
 
-# rdist, x = [], 1
-# perms.each do |perm|
-# 	rdist << PDist.rdist(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, rdist, runs, param_types, 'big_plot_r', 'R distance (normalized compliment)', title)
+rdist, x = [], 1
+perms.each do |perm|
+	rdist << PDist.rdist(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, rdist, runs, param_types, 'big_plot_r', 'R distance (normalized compliment)', title)
 
-# lcs, x = [], 1
-# perms.each do |perm|
-# 	lcs << PDist.lcs(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, lcs, runs, param_types, 'big_plot_lcs', 'Longest common subsequence (normalized)', title)
+lcs, x = [], 1
+perms.each do |perm|
+	lcs << PDist.lcs(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, lcs, runs, param_types, 'big_plot_lcs', 'Longest common subsequence (normalized)', title)
 
-# kt, x = [], 1
-# perms.each do |perm|
-# 	kt << PDist.kendalls_tau(original_order, perm)
-# 	puts "Permutations: #{x}"; x+=1
-# end
-# UPlot.uplot(dataset, gens, kt, runs, param_types, 'big_plot_kendalls', 'Kendall\'s tau (normalized)', title)
+kt, x = [], 1
+perms.each do |perm|
+	kt << PDist.kendalls_tau(original_order, perm)
+	puts "Permutations: #{x}"; x+=1
+end
+UPlot.uplot(dataset, gens, kt, runs, param_types, 'big_plot_kendalls', 'Kendall\'s tau (normalized)', title)
 
 WriteIt.write_txt("arabidopsis_datasets/#{dataset}/gens", gens)
 WriteIt.write_txt("arabidopsis_datasets/#{dataset}/runs", runs)
 WriteIt.write_txt("arabidopsis_datasets/#{dataset}/param_types", param_types)
 WriteIt.write_txt("arabidopsis_datasets/#{dataset}/fits", fits)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/dev", deviation_distances)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/sq", square)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/ham", hamming)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/rdist", rdist)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/lcs", lcs)
-# WriteIt.write_txt("arabidopsis_datasets/#{dataset}/kt", kt)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/dev", deviation_distances)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/sq", square)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/ham", hamming)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/rdist", rdist)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/lcs", lcs)
+WriteIt.write_txt("arabidopsis_datasets/#{dataset}/kt", kt)
