456f32321c958ea379008838e9b4529715942b51
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-18 18:19:38 +0100

    just create the csv in cluster

--- multiple_runs_performance.rb
+++ multiple_runs_performance.rb
@@ -5,8 +5,8 @@ dataset = ARGV[0]
 
 title = 'Many replicate runs of GATOC with varying parameter groupings'
 
-# UPlot.data_save(dataset)
-['fitness', 'dev', 'square', 'ham', 'r_dist', 'lcs', 'kt'].each do |metric|
-	filename = "umbrella_plot_#{metric}"
-	UPlot.uplot(dataset, filename, metric, title)
-end
+UPlot.data_save(dataset)
+# ['fitness', 'dev', 'square', 'ham', 'r_dist', 'lcs', 'kt'].each do |metric|
+# 	filename = "umbrella_plot_#{metric}"
+# 	UPlot.uplot(dataset, filename, metric, title)
+# end
