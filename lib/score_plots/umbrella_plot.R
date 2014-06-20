4d92c52dd94335bec4646a632f251552eacd0b26
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-20 11:06:44 +0100

    plots for some of the 10K_dataset4 runs

--- lib/score_plots/umbrella_plot.R
+++ lib/score_plots/umbrella_plot.R
@@ -8,7 +8,7 @@ av_sd <- function(df, metric){
 
 uplot <- function(df, title, metric){
   df <- av_sd(df, metric)
-  print(metric)
+  df$param_types = factor(df$param_types, levels=c('p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12'))
 	library(ggplot2)
 	p <- ggplot(df, aes(colour = replicates, y = metric_scores, x = gen)) +
 	    xlab("Generations") +
@@ -16,10 +16,11 @@ uplot <- function(df, title, metric){
 	    ggtitle(title) +
 	    scale_y_continuous(limits=c(0, 1)) +
 		scale_x_continuous() +
-		geom_line(aes(y = Average), size=1) +
-		geom_line(aes(y = Average-x), size=0.5, linetype="dotted") +
-		geom_line(aes(y = Average+x), size=0.5, linetype="dotted") +
-	    facet_grid(param_types~., scales = "free_y", space = "fixed") +
+		geom_line(aes(y = Average), size=0.5) +
+		geom_line(aes(y = Average-x), size=0.1, linetype="solid") +
+		geom_line(aes(y = Average+x), size=0.1, linetype="solid") +
+    	# geom_ribbon(aes(y = Average, ymin = (Average-x), ymax = (Average+x), fill = replicates, alpha = 0.0)) +
+    	facet_wrap(~param_types, ncol=3) +
 	    theme_bw()
 	return(p)
 }
--- multiple_runs_performance.rb
+++ multiple_runs_performance.rb
@@ -2,14 +2,19 @@
 require_relative 'lib/score_plots/umbrella_plot'
 
 dataset = ARGV[0]
+method = ARGV[1]
 
 title = 'Many replicate runs of GATOC with varying parameter groupings'
 
-UPlot.data_save(dataset)
+if method == 'csv' || method == 'both'
+	UPlot.data_save(dataset)
+end
 
-# shorts, x = ['fits','dev','square','ham','r_dist','lcs','kt'], 0
-# ['Fitness','Deviation','Square','Hamming','R','LCS','KT'].each do |metric|
-# 	filename = "umbrella_plot_#{[shorts[x]]}"
-# 	UPlot.uplot(dataset, filename, metric, title)
-# 	x+=1
-# end
+if method == 'plot' || method == 'both'
+	shorts, x = ['fits','dev','square','ham','r_dist','lcs','kt'], 0
+	['Fitness','Deviation','Square','Hamming','R','LCS','KT'].each do |metric|
+		filename = "umbrella_plot_#{[shorts[x]]}"
+		UPlot.uplot(dataset, filename, metric, title)
+		x+=1
+	end
+end
