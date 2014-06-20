ec0aa87039fa30f79c2fd1aa933565287a30c5b9
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-20 13:20:40 +0100

    can get gif images correctly ordered

--- magick.sh
+++ magick.sh
@@ -1,1 +1,5 @@
-convert */best_permutation_distribution_hyp.png images.gif
+ls Gen*/best_permutation_distribution_hyp.png | sort -k2 -tn -n > gif_loc_hyp.txt
+ls Gen*/best_permutation_ratios_10Kdiv.png | sort -k2 -tn -n > gif_loc_rat.txt
+
+convert @gif_loc_hyp.txt images_hyp.gif
+convert @gif_loc_rat.txt images_ratios.gif
