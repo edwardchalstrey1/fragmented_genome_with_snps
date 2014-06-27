fc877895201232411e919a30ae5220bc8507f060
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-27 16:32:54 +0100

    started adjacent swaps idea for testing whether fitness works, not working yet

--- fitness_method_test.rb
+++ fitness_method_test.rb
@@ -6,34 +6,70 @@ require 'pmeth'
 		identifies permuations that are approaching the correct order.
 
 		1. Get an array of correctly ordered FASTA contigs
-		2. Create permutations that are progressively further from the correct
-		3. Save the permutation fitness scores directly into a csv, to make a ggplot (but can also save permutation txt files - possibly not neccesary)
-		4. Create a plot to represent this
+		2. Get the ratio of the correctly ordered contigs, so we can workin out fitness of permutation
+		3. Create permutations that are progressively further from the correct
+		4. Save the permutation fitness scores directly into a csv, to make a ggplot (but can also save permutation txt files - possibly not neccesary)
+		5. Create a plot to represent this
 =end
 
 dataset = ARGV[0]
 size = ARGV[1] # size of each population of permuations that are progressively further from correct
-pop_num = ARGV[2] # number of populations 
+pop_num = ARGV[2] # number of populations
+div = ARGV[3] # number of divisions of the genome at which to calculate the ratio
+metric = ARGV[4]
 
-# 1
-fasta = ReformRatio::fasta_array("arabidopsis_datasets/#{dataset}/frags.fasta") # correct permutation
-###
+def adjacent_swaps_csv(dataset, size, pop_num, div, metric)
+	# 1
+	fasta = ReformRatio::fasta_array("arabidopsis_datasets/#{dataset}/frags.fasta") # correct permutation
+	###
 
-# 2/3: Population of adjacent_swap mutants (of the correct contig order)
-start_pop = []
-size.times do
+	# 2
+	snp_data = ReformRatio::get_snp_data("arabidopsis_datasets/#{dataset}/snps.vcf")
+	genome_length = ReformRatio::genome_length("arabidopsis_datasets/#{dataset}/frags.fasta")
+	ht, hm = ReformRatio.perm_pos(fasta, snp_data)
+	comparable_ratio = FitnessScore::ratio(hm, ht, div, genome_length)
+	###
+
+	# 3/4: Population of adjacent_swap mutants (of the correct contig order)
+	start_pop = []
+	size.times do
 	start_pop << fasta
-end
-
-pop_num.times do
-	adj_pop = []
-	start_pop.each do |perm|
-		new_perm = PMeth.adjacent_swap(perm)
-		adj_pop << new_perm
-		# TODO get the permutation's fitness
-		# TODO add the fitness to a csv
 	end
-	start_pop = adj_pop
+
+	WriteIt.add_to("arabidopsis_datasets/#{dataset}/adjacent_swaps.csv", "population,#{metric}")
+	x = 1
+	pop_num.times do
+		adj_pop = []
+		start_pop.each do |perm|
+			new_perm = PMeth.adjacent_swap(perm)
+			adj_pop << new_perm # need this population to be the next starting population
+			if metric == 'fitness'
+				score = GATOC.fitness(perm, snp_data, comparable_ratio, div, genome_length)
+			end
+			WriteIt.add_to("arabidopsis_datasets/#{dataset}/adjacent_swaps.csv", "#{x},#{score}")
+			# TODO add distance metrics
+		end
+		start_pop = adj_pop
+		x+=1
+	end
 end
 
-# 4:
+# 5:
+
+def metric_test_plot(dataset, filename, metric, y_axis, title)
+	myr = RinRuby.new(echo = false)
+	myr.dataset = dataset
+	myr.filename = filename
+	myr.metric = metric
+	myr.title = title
+	myr.y_axis = y_axis
+	myr.eval "source('~/fragmented_genome_with_snps/lib/score_plots/umbrella_plot.R')"
+	myr.eval "df <- read.csv(file.path(paste('~/fragmented_genome_with_snps/arabidopsis_datasets/', dataset, sep=''), adjacent_swaps.csv))"
+	myr.eval "p <- metric_test_plot(df, title, y_axis, metric)"
+	myr.eval "ggsave(p, file = paste('~/fragmented_genome_with_snps/arabidopsis_datasets/', dataset,'/', filename,'.png', sep = ''))"
+	myr.quit
+	puts 'made a plot'
+end
+
+adjacent_swaps_csv(dataset, size, pop_num, div, metric)
+metric_test_plot(dataset, 'adj_test', metric, metric, 'adjacent swaps')
