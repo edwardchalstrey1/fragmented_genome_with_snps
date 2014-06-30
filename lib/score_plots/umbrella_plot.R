6d9ffd9fc316a6e20bc4526a374a932f209bb336
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-06-30 13:22:46 +0100

    metric test plot works with geom_ribbon

--- lib/score_plots/umbrella_plot.R
+++ lib/score_plots/umbrella_plot.R
@@ -7,8 +7,8 @@ av_sd <- function(df, metric){
 }
 
 uplot <- function(df, title, y_axis){
-  df$param_types = factor(df$param_types, levels=c('p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12'))
-  df$replicates<- as.numeric(sub("p_run","",df[,2]))
+	df$param_types = factor(df$param_types, levels=c('p1','p2','p3','p4','p5','p6','p7','p8','p9','p10','p11','p12'))
+	df$replicates<- as.numeric(sub("p_run","",df[,2]))
 	library(ggplot2)
 	p <- ggplot(df, aes(colour = factor(replicates), y = metric_scores, x = gen)) +
 	    xlab("Generations of genetic algorithm - each generation is a new population of contig permutations") +
@@ -26,4 +26,27 @@ uplot <- function(df, title, y_axis){
 		guides(col = guide_legend(keywidth = 0.25, keyheight = 0.25, ncol = 4, byrow = TRUE, title.theme = element_text(size=8, angle = 0),
                               title = "Replicates"))
 	return(p)
+}
+
+av_sd2 <- function(df, metric){
+  names(df)[names(df)==metric] <- "metric_scores"
+  df <- transform(df, Average = ave(metric_scores, population))
+  sd = aggregate(df$metric_scores, by=list(population=df$population), sd)
+  df <- merge(df,sd)
+  return(df)
+}
+
+metric_test_plot <- function(df, title, y_axis, metric){
+	df <- av_sd2(df, metric)
+	library(ggplot2)
+	p <- ggplot(df, aes(y = metric_scores, x = population)) +
+	    xlab('Number of adjacent swaps') + # TODO improve axis names and title
+	    ylab(y_axis) +
+	    ggtitle(title) +
+	    scale_y_continuous(limits=c(0, 1.2)) +
+		scale_x_continuous() +
+	  geom_line(aes(y = Average), size=0.5) +
+	  geom_ribbon(aes(y = Average, ymin = (Average-x), ymax = (Average+x), fill="red", alpha=0.1)) +
+	    theme_bw()
+  return(p)
 }
