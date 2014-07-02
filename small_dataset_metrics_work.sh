ce712867dc6b80f314c429eed203fff24819ff30
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-02 13:35:53 +0100

    put the arguments the right way around, now generating 2000 populations od size 10

--- small_dataset_metrics_work.sh
+++ small_dataset_metrics_work.sh
@@ -1,7 +1,7 @@
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both Fitness 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both DeviationDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both SquareDeviationDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both HammingDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both RDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both LongestCommonSubsequence 1
-ruby do_metrics_work.rb small_dataset2a 100 1000 10 both KendallsTau 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both Fitness 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both DeviationDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both SquareDeviationDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both HammingDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both RDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both LongestCommonSubsequence 1
+ruby do_metrics_work.rb small_dataset2a 100 10 2000 both KendallsTau 1
