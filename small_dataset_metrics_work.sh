3d6440053a52afa293baab9069319c694c214bdf
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-03 17:30:09 +0100

    make 10,000 swaps for metrics

--- small_dataset_metrics_work.sh
+++ small_dataset_metrics_work.sh
@@ -1,7 +1,6 @@
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both Fitness 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both DeviationDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both SquareDeviationDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both HammingDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both RDistance 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both LongestCommonSubsequence 1
-ruby do_metrics_work.rb small_dataset2a 100 10 2000 both KendallsTau 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both DeviationDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both SquareDeviationDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both HammingDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both RDistance 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both LongestCommonSubsequence 1
+ruby do_metrics_work.rb small_dataset2a 100 10 10000 both KendallsTau 1
