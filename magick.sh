8e0e0d607177dcb93fe2de800b3fd15862397c76
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-04 12:17:12 +0100

    first test of new fitness method with gifs

--- magick.sh
+++ magick.sh
@@ -1,5 +1,8 @@
 ls Gen*/best_permutation_distribution_hyp.png | sort -k2 -tn -n > gif_loc_hyp.txt
-ls Gen*/best_permutation_ratios_10Kdiv.png | sort -k2 -tn -n > gif_loc_rat.txt
+ls Gen*/best_permutation_ratios_0.1Kdiv.png | sort -k2 -tn -n > gif_loc_rat.txt # add command line argument
 
 convert @gif_loc_hyp.txt images_hyp.gif
 convert @gif_loc_rat.txt images_ratios.gif
+
+ls Gen*/best_permutation_distribution_hm.png | sort -k2 -tn -n > gif_loc_hm.txt
+convert @gif_loc_hm.txt images_hm.gif
