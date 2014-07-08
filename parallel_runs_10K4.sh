cc640478b40dafc6c50bf79f7d6f1a37460030dc
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-08 14:59:51 +0100

    new fitness method experiment

--- parallel_runs_10K4.sh
+++ parallel_runs_10K4.sh
@@ -1,7 +1,7 @@
 <<'COMMENT'
 
-This script will run algorithm_test.rb 120 times with varying parameters.
-ARGV Parameters: dataset, run, gen, pop_size, select_num, c_mut, s_mut, save, ran, div (each explained in algorithm_test.rb).
+This script will run algorithm_test.rb 40 times with varying parameters (4 parameter groups).
+ARGV Parameters: dataset, run, gen, pop_size, select_num, c_mut, s_mut, save, ran (each explained in algorithm_test.rb).
 Each for loop contains runs of the algorithm with a different div value
 
 COMMENT
@@ -13,24 +13,8 @@ Main 120
 
 for i in {1..10}
 do
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$i 10000000000 100 50 35 35 25 5 1000.0 restart"
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+10)) 10000000000 20 10 7 7 5 1 1000.0 restart"
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+20)) 10000000000 50 25 20 20 6 4 1000.0 restart"
-	bsub -q TSL-Test256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+30)) 10000000000 20 4 9 9 1 1 1000.0 restart"
-done
-
-for i in {41..50}
-do
-	bsub -q TSL-Test256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$i 10000000000 100 50 35 35 25 5 10000.0 restart"
-	bsub -q TSL-Test256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+10)) 10000000000 20 10 7 7 5 1 10000.0 restart"
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+20)) 10000000000 50 25 20 20 6 4 10000.0 restart"
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+30)) 10000000000 20 4 9 9 1 1 10000.0 restart"
-done
-
-for i in {81..90}
-do
-	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$i 10000000000 100 50 35 35 25 5 100000.0 restart"
-	bsub -q TSL-Prod256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+10)) 10000000000 20 10 7 7 5 1 100000.0 restart"
-	bsub -q TSL-Prod256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+20)) 10000000000 50 25 20 20 6 4 100000.0 restart"
-	bsub -q TSL-Prod256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+30)) 10000000000 20 4 9 9 1 1 100000.0 restart"
+	bsub -q TSL-Test128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$i 10000000000 100 50 35 35 25 5"
+	bsub -q TSL-Prod128 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+10)) 10000000000 20 10 7 7 5 1"
+	bsub -q TSL-Test256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+20)) 10000000000 50 25 20 20 6 4"
+	bsub -q TSL-Prod256 -We 10000 "ruby algorithm_test.rb 10K_dataset4a p_run$(($i+30)) 10000000000 20 4 9 9 1 1"
 done
