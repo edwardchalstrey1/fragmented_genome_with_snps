77f32ea064ae48cb6444cd457fcd9989049e8c56
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-17 10:29:40 +0100

    added n argument to find_peaks

--- lib/locate_mutation.rb
+++ lib/locate_mutation.rb
@@ -3,11 +3,14 @@
 class LocateMutation
 	require 'rinruby'
 
-	def self.find_peak(snps)
+	# Input 0: List of SNP positions
+	# Input 1: The number of equally spaced points at which the density is to be estimated. Specify n as a power of two.
+	def self.find_peak(snps, n)
 		myr = RinRuby.new(echo=false)
+		myr.n = n
 		myr.snps = snps
 		myr.eval 'library(pracma)'
-		myr.eval 'kernel_density <- density(snps)'
+		myr.eval 'kernel_density <- density(snps, n=n)'
 		myr.eval 'index <- match(max(kernel_density$y),kernel_density$y)'
 		myr.eval 'peak <- kernel_density$x[index]'
 		peak = myr.pull 'peak'
--- test/unit_tests_locate_mutation.rb
+++ test/unit_tests_locate_mutation.rb
@@ -6,7 +6,7 @@ class TestLocateMutation < Test::Unit::TestCase
 
 	def test_find_peak
 		snps = [105,109,87,96,110,95,100,88,110,92]
-		assert_equal(93.84763, ('%.5f' % LocateMutation.find_peak(snps)).to_f)
+		assert_equal(93.84763, ('%.5f' % LocateMutation.find_peak(snps, 512)).to_f)
 	end
 
 end
