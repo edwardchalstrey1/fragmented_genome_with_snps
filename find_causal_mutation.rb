5f22043f896a41fa1edc08d38184afd748ad5d19
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-07-18 16:05:54 +0100

    can find causal mutation based on hyp SNPs peak, should perhaps modify to give the candidate SNPs in the highest ratio div

--- find_causal_mutation.rb
+++ find_causal_mutation.rb
@@ -8,20 +8,32 @@ require_relative 'lib/fitness_score'
 dataset = ARGV[0]
 run = ARGV[1]
 gen = ARGV[2]
+n = 1048576*4
 
 genome_length = ReformRatio::genome_length("arabidopsis_datasets/#{dataset}/frags.fasta")
+div = 10000
 
 Dir.chdir(File.join(Dir.home, "fragmented_genome_with_snps/arabidopsis_datasets/#{dataset}")) do
 	
-	hm = WriteIt.file_to_ints_array("hm_snps.txt")
+	hm = WriteIt.file_to_ints_array("hm_snps.txt") # Get SNP distributions
 	ht = WriteIt.file_to_ints_array("ht_snps.txt")
-	comparable_ratio = FitnessScore::ratio(hm, ht, 100, genome_length)
-	hyp = SNPdist.hyp_snps(comparable_ratio, genome_length)
 
-	puts LocateMutation.find_peak(hm, 1024)#1048576*2)
+	ratios = FitnessScore::ratio(hm, ht, div, genome_length) # Calculate homozygous/heterozygous ratio and make approximate distribution
+	hyp = SNPdist.hyp_snps(ratios, genome_length)
 
-	# perm_hm = WriteIt.file_to_ints_array("p_run#{run}/Gen#{gen}_lists/gen_#{gen}_hm.txt")
+	peak =  LocateMutation.find_peak(hyp, n) # Find the peak in the approximated (hypothetical SNP) distribution
+	causal = LocateMutation.closest_snp(peak, hm)
 
-	# puts LocateMutation.find_peak(perm_hm, n)
+	perm_hm = WriteIt.file_to_ints_array("p_run#{run}/Gen#{gen}_lists/gen_#{gen}_hm.txt")
+	perm_ht = WriteIt.file_to_ints_array("p_run#{run}/Gen#{gen}_lists/gen_#{gen}_ht.txt")
+
+	perm_ratios = FitnessScore::ratio(perm_hm, perm_ht, div, genome_length) # Calculate homozygous/heterozygous ratio and make approximate distribution
+	perm_hyp = SNPdist.hyp_snps(perm_ratios, genome_length)
+
+	perm_peak = LocateMutation.find_peak(perm_hyp, n)
+	candidate = LocateMutation.closest_snp(perm_peak, perm_hm)
+
+	puts causal
+	puts candidate
 
 end
